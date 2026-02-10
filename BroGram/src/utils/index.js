0dbc4f7f7e3373b06e8bef24e7654691fadfac33
BK0SS <kosulin.b@mail.ru>
2026-02-09 20:06:47 -0800

    start

---
+++ BroGram/src/utils/index.js
@@ -0,0 +1,405 @@
+export const workoutProgram = {
+    0: {
+        warmup: [
+            { name: "Around the worlds", sets: 2, reps: 25 },
+            { name: "Scapula pushups", sets: 2, reps: 25 }
+        ],
+        workout: [
+            { name: "Dumbbell chest press", sets: 3, reps: 10 },
+            { name: "Incline dumbbell press", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 }
+        ]
+    },
+    1: {
+        warmup: [
+            { name: "Glute bridges", sets: 2, reps: 30 },
+            { name: "Bodyweight squats", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Dumbbell shoulder press", sets: 3, reps: 10 },
+            { name: "Squats", sets: 3, reps: 10 },
+            { name: "Rear delt extensions", sets: 3, reps: 15 },
+            { name: "Leg extensions", sets: 3, reps: 12 },
+            { name: "Shoulder flyes", sets: 3, reps: 15 },
+            { name: "Seated leg curls", sets: 3, reps: 12 }
+        ]
+    },
+    2: {
+        warmup: [
+            { name: "Wall bodyweight rows", sets: 2, reps: 25 },
+            { name: "Scapula pull ups", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Lat pulldowns", sets: 3, reps: 10 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 },
+            { name: "Seated rows", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 }
+        ]
+    },
+    3: {
+        warmup: [
+            { name: "Around the worlds", sets: 2, reps: 25 },
+            { name: "Scapula pushups", sets: 2, reps: 25 }
+        ],
+        workout: [
+            { name: "Dumbbell chest press", sets: 3, reps: 10 },
+            { name: "Incline dumbbell press", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 }
+        ]
+    },
+    4: {
+        warmup: [
+            { name: "Glute bridges", sets: 2, reps: 30 },
+            { name: "Bodyweight squats", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Dumbbell shoulder press", sets: 3, reps: 10 },
+            { name: "Squats", sets: 3, reps: 10 },
+            { name: "Rear delt extensions", sets: 3, reps: 15 },
+            { name: "Leg extensions", sets: 3, reps: 12 },
+            { name: "Shoulder flyes", sets: 3, reps: 15 },
+            { name: "Seated leg curls", sets: 3, reps: 12 }
+        ]
+    },
+    5: {
+        warmup: [
+            { name: "Wall bodyweight rows", sets: 2, reps: 25 },
+            { name: "Scapula pull ups", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Lat pulldowns", sets: 3, reps: 10 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 },
+            { name: "Seated rows", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 }
+        ]
+    },
+    6: {
+        warmup: [
+            { name: "Around the worlds", sets: 2, reps: 25 },
+            { name: "Scapula pushups", sets: 2, reps: 25 }
+        ],
+        workout: [
+            { name: "Dumbbell chest press", sets: 3, reps: 10 },
+            { name: "Incline dumbbell press", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 }
+        ]
+    },
+    7: {
+        warmup: [
+            { name: "Glute bridges", sets: 2, reps: 30 },
+            { name: "Bodyweight squats", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Dumbbell shoulder press", sets: 3, reps: 10 },
+            { name: "Squats", sets: 3, reps: 10 },
+            { name: "Rear delt extensions", sets: 3, reps: 15 },
+            { name: "Leg extensions", sets: 3, reps: 12 },
+            { name: "Shoulder flyes", sets: 3, reps: 15 },
+            { name: "Seated leg curls", sets: 3, reps: 12 }
+        ]
+    },
+    8: {
+        warmup: [
+            { name: "Wall bodyweight rows", sets: 2, reps: 25 },
+            { name: "Scapula pull ups", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Lat pulldowns", sets: 3, reps: 10 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 },
+            { name: "Seated rows", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 }
+        ]
+    },
+    9: {
+        warmup: [
+            { name: "Around the worlds", sets: 2, reps: 25 },
+            { name: "Scapula pushups", sets: 2, reps: 25 }
+        ],
+        workout: [
+            { name: "Dumbbell chest press", sets: 3, reps: 10 },
+            { name: "Incline dumbbell press", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 }
+        ]
+    },
+    10: {
+        warmup: [
+            { name: "Glute bridges", sets: 2, reps: 30 },
+            { name: "Bodyweight squats", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Dumbbell shoulder press", sets: 3, reps: 10 },
+            { name: "Squats", sets: 3, reps: 10 },
+            { name: "Rear delt extensions", sets: 3, reps: 15 },
+            { name: "Leg extensions", sets: 3, reps: 12 },
+            { name: "Shoulder flyes", sets: 3, reps: 15 },
+            { name: "Seated leg curls", sets: 3, reps: 12 }
+        ]
+    },
+    11: {
+        warmup: [
+            { name: "Wall bodyweight rows", sets: 2, reps: 25 },
+            { name: "Scapula pull ups", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Lat pulldowns", sets: 3, reps: 10 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 },
+            { name: "Seated rows", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 }
+        ]
+    },
+    12: {
+        warmup: [
+            { name: "Around the worlds", sets: 2, reps: 25 },
+            { name: "Scapula pushups", sets: 2, reps: 25 }
+        ],
+        workout: [
+            { name: "Dumbbell chest press", sets: 3, reps: 10 },
+            { name: "Incline dumbbell press", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 }
+        ]
+    },
+    13: {
+        warmup: [
+            { name: "Glute bridges", sets: 2, reps: 30 },
+            { name: "Bodyweight squats", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Dumbbell shoulder press", sets: 3, reps: 10 },
+            { name: "Squats", sets: 3, reps: 10 },
+            { name: "Rear delt extensions", sets: 3, reps: 15 },
+            { name: "Leg extensions", sets: 3, reps: 12 },
+            { name: "Shoulder flyes", sets: 3, reps: 15 },
+            { name: "Seated leg curls", sets: 3, reps: 12 }
+        ]
+    },
+    14: {
+        warmup: [
+            { name: "Wall bodyweight rows", sets: 2, reps: 25 },
+            { name: "Scapula pull ups", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Lat pulldowns", sets: 3, reps: 10 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 },
+            { name: "Seated rows", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 }
+        ]
+    },
+    15: {
+        warmup: [
+            { name: "Around the worlds", sets: 2, reps: 25 },
+            { name: "Scapula pushups", sets: 2, reps: 25 }
+        ],
+        workout: [
+            { name: "Dumbbell chest press", sets: 3, reps: 10 },
+            { name: "Incline dumbbell press", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 }
+        ]
+    },
+    16: {
+        warmup: [
+            { name: "Glute bridges", sets: 2, reps: 30 },
+            { name: "Bodyweight squats", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Dumbbell shoulder press", sets: 3, reps: 10 },
+            { name: "Squats", sets: 3, reps: 10 },
+            { name: "Rear delt extensions", sets: 3, reps: 15 },
+            { name: "Leg extensions", sets: 3, reps: 12 },
+            { name: "Shoulder flyes", sets: 3, reps: 15 },
+            { name: "Seated leg curls", sets: 3, reps: 12 }
+        ]
+    },
+    17: {
+        warmup: [
+            { name: "Wall bodyweight rows", sets: 2, reps: 25 },
+            { name: "Scapula pull ups", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Lat pulldowns", sets: 3, reps: 10 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 },
+            { name: "Seated rows", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 }
+        ]
+    },
+    18: {
+        warmup: [
+            { name: "Around the worlds", sets: 2, reps: 25 },
+            { name: "Scapula pushups", sets: 2, reps: 25 }
+        ],
+        workout: [
+            { name: "Dumbbell chest press", sets: 3, reps: 10 },
+            { name: "Incline dumbbell press", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 }
+        ]
+    },
+    19: {
+        warmup: [
+            { name: "Glute bridges", sets: 2, reps: 30 },
+            { name: "Bodyweight squats", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Dumbbell shoulder press", sets: 3, reps: 10 },
+            { name: "Squats", sets: 3, reps: 10 },
+            { name: "Rear delt extensions", sets: 3, reps: 15 },
+            { name: "Leg extensions", sets: 3, reps: 12 },
+            { name: "Shoulder flyes", sets: 3, reps: 15 },
+            { name: "Seated leg curls", sets: 3, reps: 12 }
+        ]
+    },
+    20: {
+        warmup: [
+            { name: "Wall bodyweight rows", sets: 2, reps: 25 },
+            { name: "Scapula pull ups", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Lat pulldowns", sets: 3, reps: 10 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 },
+            { name: "Seated rows", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 }
+        ]
+    },
+    21: {
+        warmup: [
+            { name: "Around the worlds", sets: 2, reps: 25 },
+            { name: "Scapula pushups", sets: 2, reps: 25 }
+        ],
+        workout: [
+            { name: "Dumbbell chest press", sets: 3, reps: 10 },
+            { name: "Incline dumbbell press", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 }
+        ]
+    },
+    22: {
+        warmup: [
+            { name: "Glute bridges", sets: 2, reps: 30 },
+            { name: "Bodyweight squats", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Dumbbell shoulder press", sets: 3, reps: 10 },
+            { name: "Squats", sets: 3, reps: 10 },
+            { name: "Rear delt extensions", sets: 3, reps: 15 },
+            { name: "Leg extensions", sets: 3, reps: 12 },
+            { name: "Shoulder flyes", sets: 3, reps: 15 },
+            { name: "Seated leg curls", sets: 3, reps: 12 }
+        ]
+    },
+    23: {
+        warmup: [
+            { name: "Wall bodyweight rows", sets: 2, reps: 25 },
+            { name: "Scapula pull ups", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Lat pulldowns", sets: 3, reps: 10 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 },
+            { name: "Seated rows", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 }
+        ]
+    },
+    24: {
+        warmup: [
+            { name: "Around the worlds", sets: 2, reps: 25 },
+            { name: "Scapula pushups", sets: 2, reps: 25 }
+        ],
+        workout: [
+            { name: "Dumbbell chest press", sets: 3, reps: 10 },
+            { name: "Incline dumbbell press", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 }
+        ]
+    },
+    25: {
+        warmup: [
+            { name: "Glute bridges", sets: 2, reps: 30 },
+            { name: "Bodyweight squats", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Dumbbell shoulder press", sets: 3, reps: 10 },
+            { name: "Squats", sets: 3, reps: 10 },
+            { name: "Rear delt extensions", sets: 3, reps: 15 },
+            { name: "Leg extensions", sets: 3, reps: 12 },
+            { name: "Shoulder flyes", sets: 3, reps: 15 },
+            { name: "Seated leg curls", sets: 3, reps: 12 }
+        ]
+    },
+    26: {
+        warmup: [
+            { name: "Wall bodyweight rows", sets: 2, reps: 25 },
+            { name: "Scapula pull ups", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Lat pulldowns", sets: 3, reps: 10 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 },
+            { name: "Seated rows", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 }
+        ]
+    },
+    27: {
+        warmup: [
+            { name: "Around the worlds", sets: 2, reps: 25 },
+            { name: "Scapula pushups", sets: 2, reps: 25 }
+        ],
+        workout: [
+            { name: "Dumbbell chest press", sets: 3, reps: 10 },
+            { name: "Incline dumbbell press", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 }
+        ]
+    },
+    28: {
+        warmup: [
+            { name: "Glute bridges", sets: 2, reps: 30 },
+            { name: "Bodyweight squats", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Dumbbell shoulder press", sets: 3, reps: 10 },
+            { name: "Squats", sets: 3, reps: 10 },
+            { name: "Rear delt extensions", sets: 3, reps: 15 },
+            { name: "Leg extensions", sets: 3, reps: 12 },
+            { name: "Shoulder flyes", sets: 3, reps: 15 },
+            { name: "Seated leg curls", sets: 3, reps: 12 }
+        ]
+    },
+    29: {
+        warmup: [
+            { name: "Wall bodyweight rows", sets: 2, reps: 25 },
+            { name: "Scapula pull ups", sets: 2, reps: 20 }
+        ],
+        workout: [
+            { name: "Lat pulldowns", sets: 3, reps: 10 },
+            { name: "Tricep overhead extensions", sets: 3, reps: 12 },
+            { name: "Seated rows", sets: 3, reps: 10 },
+            { name: "Bicep curls", sets: 3, reps: 12 }
+        ]
+    }
+}
+
+export const exerciseDescriptions = {
+    "Around the worlds": "Hold a stick with a double shoulder-width grip. Start with the stick at your hips, then lift it over your head, keeping your arms straight, and lower it behind your back as far down as comfortable. Then slowly reverse the motion to return to the starting position.",
+    "Scapula pushups": "Begin in a high plank position. Keep your arms straight and only move your shoulder blades, bringing them together and apart in a controlled manner, as you lower your body towards the ground, and then raise it back up. Hold a kneeling position if the plank is too challenging.",
+    "Wall bodyweight rows": "Stand a foot away from a wall, lean back, and with your elbows by your sides and hands pointing forward of your body, press your body off the wall using your elbows while engaging your back muscles.",
+    "leaning rear delt holds": "Stand with your back against a wall, elbows at shoulder level, hands to the sky, and thumbs facing your ears. Engage your rear delts to press your back off the wall using your elbows.",
+    "Scapula pull ups": "Hang from a pull-up bar or lat pull down machine with straight arms. Without bending your elbows, engage your shoulder blades to lift your body slightly, then release back to hanging position.",
+    "Glute bridges": "Lie on your back with knees bent and feet on the ground. Lift your hips by contracting your glutes, creating a straight line from shoulders to knees.",
+    "Bodyweight squats": "Perform a regular squat, but pause for 2-3 seconds at the bottom before standing back up. Feet shoulder width apart.",
+    "Bodyweight RDLs": "Stand on one leg, hinge at the hips to lower your torso to horizontal while extending the other leg behind you, and return to a standing position by engaging your hamstrings and glutes. Microbend the leg still on the ground as you complete the exercise.",
+    "Dumbbell chest press": "Lie on a flat bench holding dumbbells at shoulder level. Press the weights up until your arms are fully extended, then lower them back down slowly.",
+    "Incline dumbbell press": "Lie on an incline bench and press dumbbells upwards, focusing on the upper part of your chest.",
+    "Bicep curls": "Stand holding a dumbbell in each hand with palms facing forward. Curl the weights towards your shoulders while keeping your elbows stationary.",
+    "Tricep overhead extensions": "Hold a dumbbell with both hands above your head. Lower the weight behind your head by bending your elbows, then extend your arms back to the starting position.",
+    "Dumbbell shoulder press": "Sit or stand with dumbbells at shoulder height. Press the weights overhead until your arms are extended, then lower them back to shoulder height.",
+    "Squats": "Stand with feet shoulder-width apart. Lower your hips back and down as if sitting in a chair, keeping your chest up, then drive back up to standing.",
+    "Rear delt extensions": "Bend forward at the hips with a flat back. Raise your arms straight back behind you to engage the rear deltoids.",
+    "Leg extensions": "Sit on a leg extension machine and extend your legs until they are straight, focusing on your quadriceps.",
+    "Shoulder flyes": "Stand or sit with dumbbells at your sides. Raise your arms outward to shoulder height with a slight bend in the elbows, then lower them back down.",
+    "Seated leg curls": "Sit on a leg curl machine with your legs on the padded lever. Curl your legs downwards/inwards to engage the hamstrings, then return to the start.",
+    "Lat pulldowns": "Sit at a lat pulldown machine, pull the bar down to your chest while engaging your lats, and return to the starting position.",
+    "Seated rows": "Sit at a cable machine and pull the handle towards your torso while keeping your back straight, focusing on squeezing your shoulder blades together."
+}
